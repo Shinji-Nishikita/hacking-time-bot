5b048c5153f442b814c7ae80da97c8929f75ea0c
Shinji-Nishikita <pizattu@gmail.com>
2021-04-07 15:36:28 +0900

    can get reply time

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
 from flask import Flask, request, abort
-
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -10,9 +12,6 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
-import os
-from dotenv import load_dotenv
-load_dotenv()
 
 CHANNEL_ACCESS_TOKEN = os.environ.get("CHANNEL_ACCESS_TOKEN")
 CHANNEL_SECRET = os.environ.get("CHANNEL_SECRET")
@@ -44,13 +43,22 @@ def callback():
 
     return 'OK'
 
-
+from time import time
+start = 0
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    # userID = event.source.userID
     if event.message.text == "start hacking":
         reply_message = "measuring hacking time"
+        global start
+        start = time()
     else:
-        reply_message = f"total hacking time is 0 hour 10 minutes 15 seconds"
+        end = time()
+        dif = int(end - start)
+        hour = dif // 3600
+        minute = (dif % 3600) // 60
+        second = dif % 60
+        reply_message = f"hacking time is {hour}hour{minute}minutes{second}seconds"
 
     line_bot_api.reply_message(
             event.reply_token,
