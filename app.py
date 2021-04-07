2402afee39b73af67e429ae4c47ae2c451770eb8
Shinji-Nishikita <pizattu@gmail.com>
2021-04-07 16:26:16 +0900

    converted sec to min and hour

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
-
+from time import time
 
 CHANNEL_ACCESS_TOKEN = os.environ.get("CHANNEL_ACCESS_TOKEN")
 CHANNEL_SECRET = os.environ.get("CHANNEL_SECRET")
@@ -43,27 +43,34 @@ def callback():
 
     return 'OK'
 
-from time import time
-start = 0
+
+# start = 0
+users = {}
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    # userID = event.source.userID
+    userId = event.source.user_id
     if event.message.text == "start hacking":
         reply_message = "measuring hacking time"
-        global start
-        start = time()
+        if not userId in users:
+            users[userId] = {}
+            users[userId]["total"] = 0
+        users[userId]["start"] = time()
     else:
         end = time()
-        dif = int(end - start)
+        dif = int(end - users[userId]["start"])
+        users[userId]["total"] += dif
         hour = dif // 3600
         minute = (dif % 3600) // 60
         second = dif % 60
-        reply_message = f"hacking time is {hour}hour{minute}minutes{second}seconds"
+        totalhour = users[userId]["total"] // 3600
+        totalminutes = (users[userId]["total"] % 3600) // 60 
+        totalseconds = users[userId]["total"] % 60
+        # reply_message = f"Hacking time is { hour }hour{ minute }minutes{ second }seconds. Total hacking time is {users[userId]['total']}seconds"
+        reply_message = f"Hacking time is { hour }hour{ minute }minutes{ second }seconds. Total hacking time is { totalhour }hour{ totalminutes }minutes{ totalseconds }seconds."
 
     line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=reply_message))
-
 
 if __name__ == "__main__":
     app.run()
